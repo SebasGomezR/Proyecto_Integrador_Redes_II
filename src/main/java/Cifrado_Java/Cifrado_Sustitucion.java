5386f0ead438bde6487a76f414def9c2d4da118b
SebasGomezR <sebasgom.510@gmail.com>
2023-05-22 15:16:16 -0500

    Este es mi primer commit

---
+++ src/main/java/Cifrado_Java/Cifrado_Sustitucion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cifrado_Java;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cifrado_Sustitucion {
+    
+    private char[] letras;
+    
+    
+    public void menssage(){
+        
+        letras = new char[26];
+    
+        String mensaje = "hola como estas";
+    
+        for(int i = 0; i < mensaje.length(); i++){
+            char letra = mensaje.charAt(i);
+        }
+    
+    }
+}
